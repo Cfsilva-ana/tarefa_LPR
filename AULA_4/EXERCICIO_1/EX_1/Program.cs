b75b3860ce4fe872d932693cd51ed076dafb1ecf
Ana <anaceciliadefariasilva@gmail.com>
2025-03-21 09:29:22 -0300

    Ex_1
    O programa solicita ao usuário que insira um número inteiro e verifica se a entrada é válida. Caso o usuário insira um valor que não seja um número inteiro (como letras, símbolos ou números decimais), o programa exibe uma mensagem de erro em vermelho e encerra imediatamente.
    
    Se a entrada for válida, o programa prossegue e verifica se o número digitado é par ou ímpar, exibindo o resultado na tela.

---
+++ AULA_4/EXERCICIO_1/EX_1/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("-----------------------------");
+Console.WriteLine("|                           |");
+Console.WriteLine("|         LPR AULA 4        |");
+Console.WriteLine("|                           |");
+Console.WriteLine("-----------------------------");
+Console.ResetColor();
+
+
+
+Console.Write("Insira um número inteiro para verificar se ele é par ou impar: "); 
+string Numero_interiro = Console.ReadLine();
+
+
+    if(!int.TryParse(Numero_interiro, out int numero))
+    {
+        Console.ForegroundColor = ConsoleColor.DarkRed;
+        Console.WriteLine("Entrada inválida!!!!! Encerrando o programa.");
+        Console.ResetColor();
+        Environment.Exit(0);
+    }
+
+    if (numero % 2 == 0)
+        Console.WriteLine($"O número {numero} é par.");
+    else
+        Console.WriteLine($"O número {numero} é ímpar.");
