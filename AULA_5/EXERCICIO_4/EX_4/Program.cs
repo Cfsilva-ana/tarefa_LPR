3a93c5ccb57f0ce2ebae689bc5452ed47982e5f6
Ana <anaceciliadefariasilva@gmail.com>
2025-03-28 22:15:13 -0300

    EX_4

--- AULA_5/EXERCICIO_4/EX_4/Program.cs
+++ AULA_5/EXERCICIO_4/EX_4/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine("-----------------------------");
+Console.WriteLine("|                           |");
+Console.WriteLine("|         LPR AULA 5        |");
+Console.WriteLine("|                           |");
+Console.WriteLine("-----------------------------");
+Console.ResetColor();
+
+
+int numero;
+
+// Loop para garantir que o usuário insira um número inteiro válido
+while (true)
+{
+    Console.Write("Digite um número: ");
+    string entrada = Console.ReadLine();
+
+    if (int.TryParse(entrada, out numero))
+    {
+        break; 
+    }
+    else
+    {
+        Console.WriteLine("Entrada inválida! Digite apenas números.");
+    }
+}
+
+// Calcula o quadrado do número
+int quadrado = numero * numero;
+Console.WriteLine($"O quadrado de {numero} é {quadrado}");
+
+// Calcula a soma dos dígitos do quadrado
+int somaDigitos = 0;
+int temp = quadrado;
+
+while (temp > 0)
+{
+    somaDigitos += temp % 10; // Pega o último dígito e soma
+    temp /= 10; // Remove o último dígito
+}
+
+Console.WriteLine($"A soma dos dígitos de {quadrado} é: {somaDigitos}");
+
