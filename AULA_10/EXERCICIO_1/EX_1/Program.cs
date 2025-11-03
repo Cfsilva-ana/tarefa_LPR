2569c209cde8ebe7db262aae39f61879cbd75c80
Ana <anaceciliadefariasilva@gmail.com>
2025-11-03 10:32:32 -0300

    up

---
+++ AULA_10/EXERCICIO_1/EX_1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Retangulo
+{
+    public double Altura { get; set; }
+    public double Largura { get; set; }
+
+    public double calcArea()
+    {
+        return Largura * Altura;
+    }
+
+    public double calcPerimetro()
+    {
+        return 2 * (Largura + Altura);
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Retangulo retangulo = new Retangulo();
+
+        Console.Write("Digite a largura do retângulo: ");
+        retangulo.Largura = double.Parse(Console.ReadLine());
+
+        Console.Write("Digite a altura do retângulo: ");
+        retangulo.Altura = double.Parse(Console.ReadLine());
+
+        Console.WriteLine($"Área: {retangulo.calcArea():F2}");
+        Console.WriteLine($"Perímetro: {retangulo.calcPerimetro():F2}");
+    }
+}
